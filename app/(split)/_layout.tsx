087df453b1112d684e3b7ca39f5731d3c038b0d0
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-06 20:17:24 -0400

    Stack nav for split screens

--- app/(split)/_layout.tsx
+++ app/(split)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -12,42 +12,46 @@ function TabBarIcon(props: {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function StackLayout() {
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        // headerTransparent: true,
-        // headerLargeTitle: true,
-      }}>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: 'Enter Total',
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       {({ pressed }) => (
-          //         <FontAwesome
-          //           name='gears'
-          //           size={25}
-          //           color={Colors[colorScheme ?? 'light'].text}
-          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-          //         />
-          //       )}
-          //     </Pressable>
-          //   </Link>
-          // ),
-        }}
-      />
-      <Stack.Screen
-        name="items"
-        options={{
-          title: "Eneter Items"
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar hidden />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          // headerTransparent: true,
+          // headerLargeTitle: true,
+        }}>
+
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Enter Total',
+            // headerRight: () => (
+            //   <Link href="/modal" asChild>
+            //     <Pressable>
+            //       {({ pressed }) => (
+            //         <FontAwesome
+            //           name='gears'
+            //           size={25}
+            //           color={Colors[colorScheme ?? 'light'].text}
+            //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+            //         />
+            //       )}
+            //     </Pressable>
+            //   </Link>
+            // ),
+          }}
+        />
+        <Stack.Screen
+          name="items"
+          options={{
+            title: "Eneter Items"
+          }}
+        />
+      </Stack>
+    </>
   );
 }
