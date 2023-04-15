c07758bce7d6c0cc793d1ca4332be8f68ca751b9
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-15 10:13:47 -0400

    remove tabBarIcon things, do not hide status bar

--- app/(split)/_layout.tsx
+++ app/(split)/_layout.tsx
@@ -1,22 +1,15 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
 
 export default function StackLayout() {
 
   return (
     <>
-      <StatusBar hidden />
+      {/* <StatusBar hidden /> */}
 
       <Stack
         screenOptions={{
