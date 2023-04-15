99a952ed294c799038905a25bc76f4ba5b70bd49
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-15 10:17:14 -0400

    new total screen with custom components

--- app/(split)/items.tsx
+++ app/(split)/items.tsx
@@ -1,25 +1,44 @@
-import { KeyboardAvoidingView } from "react-native";
 import { Text, View } from "../../components/Themed";
 import { useSearchParams } from "expo-router";
+import ScreenContainer from "../../components/ScreenContainer";
+import PersonWithItems from "../../components/PersonWithItems";
+import useColorScheme from "../../utils/useColorScheme";
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
 
 export default function () {
 
+  const styles = makeStyles();
   const { total } = useSearchParams();
-  // console.log(JSON.stringify(params));
-
 
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior="height"
-    >
-      <View
-        style={{ flex: 1, justifyContent: "center", alignItems: "center", }}
-      >
-        <Text>
-          {total}
-        </Text>
-      </View>
-    </KeyboardAvoidingView>
-  )
+    <ScreenContainer>
+      {/* <View
+        style={{ flex: 1 }}
+      > */}
+
+      <Text style={styles.total}>
+        {total}
+      </Text>
+
+      <PersonWithItems defaultName="Name" />
+
+      {/* </View> */}
+    </ScreenContainer>
+  );
+}
+
+
+const makeStyles = () => {
+
+  const theme = useColorScheme();
+
+  return StyleSheet.create({
+    total: {
+      textAlign: "center",
+      color: Colors[theme].text,
+      fontSize: 50,
+      fontVariant: ["tabular-nums"],
+    }
+  });
 }
