3adc6a5a1528f407d56d775a9baa1ec76dc3c162
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-16 13:01:02 -0400

    sned state callbacks to person components

--- app/(split)/items.tsx
+++ app/(split)/items.tsx
@@ -1,29 +1,66 @@
 import { Text, View } from "../../components/Themed";
 import { useSearchParams } from "expo-router";
 import ScreenContainer from "../../components/ScreenContainer";
-import PersonWithItems from "../../components/PersonWithItems";
+import PersonWithItems, { Person } from "../../components/PersonWithItems";
 import useColorScheme from "../../utils/useColorScheme";
 import { StyleSheet } from "react-native";
 import Colors from "../../constants/Colors";
+import Layout from "../../constants/Layout";
+import { LinkButton } from "../../components/Buttons";
+import { useState } from "react";
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+
+const defaultPersons: Person[] = [
+  { name: '', items: [], id: uuidv4() },
+  { name: '', items: [], id: uuidv4() },
+]
 
 export default function () {
 
   const styles = makeStyles();
   const { total } = useSearchParams();
 
+  const [persons, setPersons] = useState<Person[]>(defaultPersons);
+
+
+  // helper function, returns a function that sets persons[index] to the supplied person.
+  function setPerson(index: number) {
+    return (person: Person) => setPersons(prevPersons => [
+      ...prevPersons.slice(0, index),
+      person,
+      ...prevPersons.slice(index + 1)
+    ]);
+  }
+
   return (
     <ScreenContainer>
-      {/* <View
-        style={{ flex: 1 }}
-      > */}
 
       <Text style={styles.total}>
-        {total}
+        ${total}
       </Text>
 
-      <PersonWithItems defaultName="Name" />
+      <View style={styles.personsContainer}>
 
-      {/* </View> */}
+        {persons.map((person, index) => (
+          <PersonWithItems
+            key={person.id}
+            defaultName={`Person ${index + 1}`}
+            person={person}
+            setPerson={setPerson(index)}
+          />
+        ))}
+
+      </View>
+
+      <LinkButton
+        title="Next"
+        href={{
+          pathname: 'summary',
+          params: { persons: JSON.stringify(persons) }
+        }}
+      />
+
     </ScreenContainer>
   );
 }
@@ -39,6 +76,12 @@ const makeStyles = () => {
       color: Colors[theme].text,
       fontSize: 50,
       fontVariant: ["tabular-nums"],
+      marginBottom: Layout.margin,
+    },
+    personsContainer: {
+      flex: 1,
+      flexDirection: "row",
+      // borderColor: 'red', borderWidth: 1,
     }
   });
 }
