ebc6dd002430a4dbc5eba3a5d0d23046ecb9bd76
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-18 17:24:01 -0400

    change keyboardavoidingview behavior

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -2,26 +2,38 @@ import { ReactNode } from "react";
 import { KeyboardAvoidingView, KeyboardAvoidingViewProps } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Layout from "../constants/Layout";
+import { View } from "./Themed";
 
 export default function ({
   children,
+  style,
   ...props
 }: { children: ReactNode } & KeyboardAvoidingViewProps) {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
     <KeyboardAvoidingView
-      style={{
-        flex: 1,
-        padding: Layout.margin,
-        // borderColor: 'red', borderWidth: 2,
-        marginTop: safeAreaInsets.top,
-        marginBottom: safeAreaInsets.bottom,
-      }}
-      behavior="height"
+      style={[
+        {
+          flex: 1,
+          // padding: Layout.margin,
+          // borderColor: 'red', borderWidth: 2,
+          marginTop: safeAreaInsets.top,
+          marginBottom: safeAreaInsets.bottom,
+        },
+        style,
+      ]}
+      behavior="padding"
       {...props}
     >
-      {children}
+      <View
+        style={{
+          flex: 1,
+          padding: Layout.margin,
+        }}
+      >
+        {children}
+      </View>
     </KeyboardAvoidingView>
   );
 }
