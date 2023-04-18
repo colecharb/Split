4c2aa8088cad4e3be0303190b8dfd736e27910d0
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-18 16:57:13 -0400

    prettier formatting, added subtle color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,19 +3,21 @@ const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
-    medium: '#888',
-    background: '#fff',
+    text: "#000",
+    medium: "#888",
+    subtle: "#ccc",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    medium: '#888',
-    background: '#000',
+    text: "#fff",
+    medium: "#888",
+    subtle: "#444",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
