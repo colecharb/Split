49cc8dd3e9881109c0c9ddb4dada7a2aa62cdcaf
colecharb <40375497+colecharb@users.noreply.github.com>
2023-04-18 16:57:00 -0400

    prettier formatting

--- components/PersonWithItems.tsx
+++ components/PersonWithItems.tsx
@@ -1,22 +1,19 @@
 import { Text, View } from "./Themed";
-import { FlatList, ListRenderItem, NativeScrollEvent, NativeSyntheticEvent, Pressable, StyleSheet, TextInput } from "react-native";
+import { FlatList, Pressable, StyleSheet, TextInput } from "react-native";
 import useColorScheme from "../utils/useColorScheme";
 import Colors from "../constants/Colors";
-import { LegacyRef, SetStateAction, useEffect, useRef, useState } from "react";
 import { Button } from "./Buttons";
 import Layout from "../constants/Layout";
-import 'react-native-get-random-values';
-import { v4 as uuidv4 } from 'uuid';
-
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+import { Person } from "../contexts/Split";
 
 export type ItemData = {
-  id: string, // for selecting and editing individual items
-  amount: number,
-}
-
+  id: string; // for selecting and editing individual items
+  amount: number;
+};
 
 function Item({ id, amount }: ItemData) {
-
   // const ref_TextInput = useRef<TextInput>(null);
 
   const styles = makeStyles();
@@ -33,56 +30,41 @@ function Item({ id, amount }: ItemData) {
       // value={amount}
       /> */}
 
-      <Text
-        style={styles.itemAmount}
-      >
-        {amount.toFixed(2)}
-      </Text>
-
+      <Text style={styles.itemAmount}>{amount.toFixed(2)}</Text>
     </Pressable>
-  )
-}
-
-
-export type Person = {
-  name: string,
-  items: ItemData[],
-  id: string,
+  );
 }
 
 type PersonWithItemsParams = {
-  defaultName: string,
-  person: Person,
-  setPerson: (person: Person) => void, // sets state in parent
-}
+  defaultName: string;
+  person: Person;
+  setPerson: (person: Person) => void; // sets state in parent
+};
 
-export default function ({ defaultName, person, setPerson }: PersonWithItemsParams) {
-
-  // const [name, setName] = useState<string>('')
-  // const [items, setItems] = useState<ItemData[]>([]);
-
+export default function ({
+  defaultName,
+  person,
+  setPerson,
+}: PersonWithItemsParams) {
   const setName = (name: string) => {
-    setPerson({ ...person, name })
-  }
+    name = name.trim();
+    setPerson({ ...person, name });
+  };
 
   const setItems = (items: ItemData[]) => {
-    setPerson({ ...person, items })
-  }
-
-  // const ref_FlatList = useRef<FlatList>(null);
+    setPerson({ ...person, items });
+  };
 
   const theme = useColorScheme();
   const styles = makeStyles();
 
   const addItem = () => {
-
     const newItem: ItemData = {
       id: uuidv4(),
-      amount: (Math.random() * 14) + 1
-    }
-
+      amount: Math.random() * 14 + 1,
+    };
     setItems([newItem, ...person.items]);
-  }
+  };
 
   const renderItem = ({ item: { id, amount } }: { item: ItemData }) => (
     <Item
@@ -97,40 +79,32 @@ export default function ({ defaultName, person, setPerson }: PersonWithItemsPara
       title="add item"
       onPress={addItem}
     />
-  )
+  );
 
   return (
-
     <View style={{ flex: 1 }}>
-
       <TextInput
         value={person.name}
         onChangeText={setName}
-        style={styles.name}
-        selectionColor={Colors[theme].tint}
         placeholder={defaultName}
         placeholderTextColor={Colors[theme].medium}
+        style={styles.name}
+        selectionColor={Colors[theme].tint}
       />
-
-      <FlatList
-        // ref={ref_FlatList}
-        data={person.items}
-        keyExtractor={item => item.id}
-        // onScrollEndDrag={onScrollEndDrag}
-
-        contentContainerStyle={styles.itemsContainer}
-
-        ListHeaderComponent={ListHeaderComponent}
-        renderItem={renderItem}
-        // ListFooterComponent={ListFooterComponent}
-      />
-
+      <View style={{ flexDirection: "row", flex: 1, justifyContent: "center" }}>
+        <FlatList
+          data={person.items}
+          keyExtractor={item => item.id}
+          contentContainerStyle={styles.itemsContainer}
+          ListHeaderComponent={ListHeaderComponent}
+          renderItem={renderItem}
+        />
+      </View>
     </View>
   );
 }
 
 const makeStyles = () => {
-
   const theme = useColorScheme();
 
   return StyleSheet.create({
@@ -141,20 +115,20 @@ const makeStyles = () => {
       textDecorationLine: "underline",
       color: Colors[theme].text,
       // margin: Layout.margin,
-
     },
     itemsContainer: {
       alignItems: "center",
       // margin: Layout.margin,
-      // borderColor: 'red', borderWidth: 1,
+      borderColor: "red",
+      borderWidth: 1,
     },
     itemAmount: {
       color: Colors[theme].text,
       fontSize: 30,
       textAlign: "center",
       fontVariant: ["tabular-nums"],
-      width: '100%',
+      width: "100%",
       // borderWidth: 1, borderColor: 'red',
-    }
+    },
   });
-}
+};
