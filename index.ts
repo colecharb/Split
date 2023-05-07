ccd69d5395a5a57b1e794ab6e9b8e135045da252
colecharb <40375497+colecharb@users.noreply.github.com>
2023-05-07 10:38:51 -0400

    handle fatal errors with alerts

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import 'expo-router/entry';
+import { Alert } from "react-native/types";
+
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  // Log the error to your preferred error tracking service
+  console.error(error, isFatal);
+
+  if (isFatal) {
+    Alert.alert("A fatal error occurred.");
+  }
+});
